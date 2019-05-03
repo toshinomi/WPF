aa3fcf0192bd9ce30ccbb9d4edc0d209af51e537
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-05-03 20:10:38 +0900

    プロジェクト ファイルを追加します。

---
+++ CSharp/ImageProcessing/EdgeDetection.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessing
+{
+    public class EdgeDetection
+    {
+        private const int m_nMaskSize = 3;
+        private uint m_nFilterMax;
+        private BitmapImage m_bitmap;
+        private WriteableBitmap m_wBitmap;
+
+        public EdgeDetection(BitmapImage _bitmap)
+        {
+            m_nFilterMax = 1;
+            m_bitmap = _bitmap;
+        }
+
+        public EdgeDetection(BitmapImage _bitmap, uint _filterMax)
+        {
+            m_nFilterMax = _filterMax;
+            m_bitmap = _bitmap;
+        }
+
+        ~EdgeDetection()
+        {
+            m_bitmap = null;
+        }
+
+        public WriteableBitmap GetBitmap()
+        {
+            return m_wBitmap;
+        }
+
+        public void SetBitmap(WriteableBitmap _bitmap)
+        {
+            m_wBitmap = _bitmap;
+        }
+
+        public bool GoEdgeDetection(CancellationToken _token)
+        {
+            bool bRst = true;
+
+            double[,] dMask =
+            {
+                {1.0,  1.0, 1.0},
+                {1.0, -8.0, 1.0},
+                {1.0,  1.0, 1.0}
+            };
+
+            int nWidthSize = m_bitmap.PixelWidth;
+            int nHeightSize = m_bitmap.PixelHeight;
+            int nMasksize = dMask.GetLength(0);
+
+            m_wBitmap = new WriteableBitmap(m_bitmap);
+            m_wBitmap.Lock();
+
+            int nIdxWidth;
+            int nIdxHeight;
+
+            unsafe
+            {
+                for (nIdxHeight = 0; nIdxHeight < nHeightSize; nIdxHeight++)
+                {
+                    for (nIdxWidth = 0; nIdxWidth < nWidthSize; nIdxWidth++)
+                    {
+                        if (_token.IsCancellationRequested)
+                        {
+                            bRst = false;
+                            break;
+                        }
+
+                        byte* pPixel = (byte*)m_wBitmap.BackBuffer + nIdxHeight * m_wBitmap.BackBufferStride + nIdxWidth * 4;
+
+                        double dCalB = 0.0;
+                        double dCalG = 0.0;
+                        double dCalR = 0.0;
+                        double dCalA = 0.0;
+                        int nIdxWidthMask;
+                        int nIdxHightMask;
+                        int nFilter = 0;
+
+                        while (nFilter < m_nFilterMax)
+                        {
+                            for (nIdxHightMask = 0; nIdxHightMask < nMasksize; nIdxHightMask++)
+                            {
+                                for (nIdxWidthMask = 0; nIdxWidthMask < nMasksize; nIdxWidthMask++)
+                                {
+                                    if (nIdxWidth + nIdxWidthMask > 0 &&
+                                        nIdxWidth + nIdxWidthMask < nWidthSize &&
+                                        nIdxHeight + nIdxHightMask > 0 &&
+                                        nIdxHeight + nIdxHightMask < nHeightSize)
+                                    {
+                                        byte* pPixel2 = (byte*)m_wBitmap.BackBuffer + (nIdxHeight + nIdxHightMask) * m_wBitmap.BackBufferStride + (nIdxWidth + nIdxWidthMask) * 4;
+
+                                        dCalB += pPixel2[(int)ComInfo.Pixel.B] * dMask[nIdxWidthMask, nIdxHightMask];
+                                        dCalG += pPixel2[(int)ComInfo.Pixel.G] * dMask[nIdxWidthMask, nIdxHightMask];
+                                        dCalR += pPixel2[(int)ComInfo.Pixel.R] * dMask[nIdxWidthMask, nIdxHightMask];
+                                        dCalA += pPixel2[(int)ComInfo.Pixel.A] * dMask[nIdxWidthMask, nIdxHightMask];
+                                    }
+                                }
+                            }
+                            nFilter++;
+                        }
+                        pPixel[(int)ComInfo.Pixel.B] = ComFunc.DoubleToByte(dCalB);
+                        pPixel[(int)ComInfo.Pixel.G] = ComFunc.DoubleToByte(dCalG);
+                        pPixel[(int)ComInfo.Pixel.R] = ComFunc.DoubleToByte(dCalR);
+                        pPixel[(int)ComInfo.Pixel.A] = ComFunc.DoubleToByte(dCalA);
+                    }
+                }
+                m_wBitmap.AddDirtyRect(new Int32Rect(0, 0, nWidthSize, nHeightSize));
+                m_wBitmap.Unlock();
+                m_wBitmap.Freeze();
+            }
+
+            return bRst;
+        }
+    }
+}
---
+++ CSharp/ImageProcessing/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageProcessing
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage m_bitmap;
+        private EdgeDetection m_edgeDetection;
+        private string m_strOpenFileName;
+        private CancellationTokenSource m_tokenSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnFileSelect.IsEnabled = true;
+            btnAllClear.IsEnabled = true;
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = false;
+
+            m_bitmap = null;
+            m_tokenSource = null;
+            m_edgeDetection = null;
+        }
+
+        ~MainWindow()
+        {
+            m_bitmap = null;
+            m_tokenSource = null;
+            m_edgeDetection = null;
+        }
+
+        public void SetButtonEnable()
+        {
+            btnFileSelect.IsEnabled = true;
+            btnAllClear.IsEnabled = true;
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+
+            return;
+        }
+
+        public void SetTextTime(long _lTime)
+        {
+            textBoxTime.Text = _lTime.ToString();
+
+            return;
+        }
+
+        private void OnClickBtnFileSelect(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+
+            openFileDlg.FileName = "default.jpg";
+            openFileDlg.InitialDirectory = @"C:\";
+            openFileDlg.Filter = "All Files(*.*)|*.*";
+            openFileDlg.FilterIndex = 1;
+            openFileDlg.Title = "Please select a file to open";
+            openFileDlg.RestoreDirectory = true;
+            openFileDlg.CheckFileExists = true;
+            openFileDlg.CheckPathExists = true;
+
+            if (openFileDlg.ShowDialog() == true)
+            {
+                pictureBoxOriginal.Source = null;
+                pictureBoxAfter.Source = null;
+                m_strOpenFileName = openFileDlg.FileName;
+                try
+                {
+                    LoadImage();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(this, "Open File Error", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                pictureBoxOriginal.Source = m_bitmap;
+                btnStart.IsEnabled = true;
+                textBoxTime.Text = "";
+            }
+            return;
+        }
+
+        public void LoadImage()
+        {
+            m_bitmap = new BitmapImage();
+            m_bitmap.BeginInit();
+            m_bitmap.UriSource = new Uri(m_strOpenFileName);
+            m_bitmap.EndInit();
+            m_bitmap.Freeze();
+
+            m_edgeDetection = new EdgeDetection(m_bitmap);
+
+            return;
+        }
+
+        private void OnClickBtnAllClear(object sender, RoutedEventArgs e)
+        {
+            pictureBoxOriginal.Source = null;
+            pictureBoxAfter.Source = null;
+
+            m_bitmap = null;
+            m_strOpenFileName = "";
+
+            textBoxTime.Text = "";
+
+            btnFileSelect.IsEnabled = true;
+            btnAllClear.IsEnabled = true;
+            btnStart.IsEnabled = false;
+
+            return;
+        }
+
+        private async void OnClickBtnStart(object sender, RoutedEventArgs e)
+        {
+            pictureBoxAfter.Source = null;
+
+            btnFileSelect.IsEnabled = false;
+            btnAllClear.IsEnabled = false;
+            btnStart.IsEnabled = false;
+
+            textBoxTime.Text = "";
+
+            LoadImage();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            btnStop.IsEnabled = true;
+            bool bResult = await TaskWorkImageProcessing();
+            if (bResult)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(m_strOpenFileName);
+                bitmap.EndInit();
+                pictureBoxOriginal.Source = bitmap;
+                pictureBoxAfter.Source = m_edgeDetection.GetBitmap();
+
+                stopwatch.Stop();
+
+                Dispatcher.Invoke(new Action<long>(SetTextTime), stopwatch.ElapsedMilliseconds);
+            }
+            Dispatcher.Invoke(new Action(SetButtonEnable));
+
+            stopwatch = null;
+            m_tokenSource = null;
+            m_bitmap = null;
+
+            return;
+        }
+
+        public async Task<bool> TaskWorkImageProcessing()
+        {
+            m_tokenSource = new CancellationTokenSource();
+            CancellationToken token = m_tokenSource.Token;
+            bool bRst = await Task.Run(() => m_edgeDetection.GoEdgeDetection(token));
+            return bRst;
+        }
+
+        private void OnClickBtnStop(object sender, RoutedEventArgs e)
+        {
+            if (m_tokenSource != null)
+            {
+                m_tokenSource.Cancel();
+            }
+
+            return;
+        }
+
+        private void OnClosingWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (m_tokenSource != null)
+            {
+                e.Cancel = true;
+            }
+
+            return;
+        }
+    }
+}

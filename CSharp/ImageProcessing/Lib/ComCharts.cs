d589e244b48e3100941ad2554070b953fe29aac2
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-25 21:08:06 +0900

    LiveChartsのライブラリ化完了

---
+++ CSharp/ImageProcessing/Lib/ComCharts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessing
+{
+    abstract class ComCharts
+    {
+        protected int[,] m_nHistgram;
+        protected BitmapImage m_bitmap;
+        protected WriteableBitmap m_wbitmap;
+
+        public ComCharts()
+        {
+            m_nHistgram = new int[2, 256];
+        }
+
+        ~ComCharts()
+        {
+        }
+
+        abstract public GraphData DrawHistgram();
+
+        public void CalHistgram()
+        {
+            int nWidthSize = m_bitmap.PixelWidth;
+            int nHeightSize = m_bitmap.PixelHeight;
+
+            WriteableBitmap wBitmap = new WriteableBitmap(m_bitmap);
+
+            int nIdxWidth;
+            int nIdxHeight;
+
+            unsafe
+            {
+                for (nIdxHeight = 0; nIdxHeight < nHeightSize; nIdxHeight++)
+                {
+                    for (nIdxWidth = 0; nIdxWidth < nWidthSize; nIdxWidth++)
+                    {
+                        byte* pPixel = (byte*)wBitmap.BackBuffer + nIdxHeight * wBitmap.BackBufferStride + nIdxWidth * 4;
+                        byte nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
+
+                        m_nHistgram[0, nGrayScale] += 1;
+
+                        if (m_wbitmap != null)
+                        {
+                            pPixel = (byte*)m_wbitmap.BackBuffer + nIdxHeight * m_wbitmap.BackBufferStride + nIdxWidth * 4;
+                            nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
+
+                            m_nHistgram[1, nGrayScale] += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void InitHistgram()
+        {
+            for (int nIdx = 0; nIdx < (m_nHistgram.Length >> 1); nIdx++)
+            {
+                m_nHistgram[0, nIdx] = 0;
+                m_nHistgram[1, nIdx] = 0;
+            }
+        }
+    }
+}
--- CSharp/ImageProcessing/Lib/ComHistgramLiveCharts.cs
+++ CSharp/ImageProcessing/Lib/ComHistgramLiveCharts.cs
@@ -9,58 +9,53 @@ using System.Windows.Media.Imaging;
 
 namespace ImageProcessing
 {
-    class ComHistgramLiveCharts
+    class ComHistgramLiveCharts : ComCharts
     {
-        private int[,] m_nHistgram;
-        private BitmapImage m_bitmap;
-        private WriteableBitmap m_wbitmap;
-
         public int[,] Histgram
         {
-            get { return m_nHistgram; }
+            get { return base.m_nHistgram; }
         }
 
         public BitmapImage Bitmap
         {
-            set { m_bitmap = value; }
-            get { return m_bitmap; }
+            set { base.m_bitmap = value; }
+            get { return base.m_bitmap; }
         }
 
         public WriteableBitmap WBitmap
         {
-            set { m_wbitmap = value; }
-            get { return m_wbitmap; }
+            set { base.m_wbitmap = value; }
+            get { return base.m_wbitmap; }
         }
 
         public ComHistgramLiveCharts()
         {
-            m_nHistgram = new int[2, 256];
         }
 
         ~ComHistgramLiveCharts()
         {
         }
 
-        public GraphData DrawHistgram()
+        public override GraphData DrawHistgram()
         {
             GraphData graphData = new GraphData();
 
-            InitHistgram();
+            base.InitHistgram();
 
-            CalHistgram();
+            base.CalHistgram();
 
             var chartValue1 = new ChartValues<int>();
             var chartValue2 = new ChartValues<int>();
-            for (int nIdx = 0; nIdx < (m_nHistgram.Length >> 1); nIdx++)
+            for (int nIdx = 0; nIdx < (base.m_nHistgram.Length >> 1); nIdx++)
             {
-                chartValue1.Add(m_nHistgram[0, nIdx]);
+                chartValue1.Add(base.m_nHistgram[0, nIdx]);
                 if (m_wbitmap == null)
                 {
                     chartValue2.Add(0);
                 }
                 else
                 {
-                    chartValue2.Add(m_nHistgram[1, nIdx]);
+                    chartValue2.Add(base.m_nHistgram[1, nIdx]);
                 }
             }
 
@@ -83,48 +78,6 @@ namespace ImageProcessing
             graphData.seriesCollection = seriesCollection;
 
             return graphData;
-        }
-
-        private void CalHistgram()
-        {
-            int nWidthSize = m_bitmap.PixelWidth;
-            int nHeightSize = m_bitmap.PixelHeight;
-
-            WriteableBitmap wBitmap = new WriteableBitmap(m_bitmap);
-
-            int nIdxWidth;
-            int nIdxHeight;
-
-            unsafe
-            {
-                for (nIdxHeight = 0; nIdxHeight < nHeightSize; nIdxHeight++)
-                {
-                    for (nIdxWidth = 0; nIdxWidth < nWidthSize; nIdxWidth++)
-                    {
-                        byte* pPixel = (byte*)wBitmap.BackBuffer + nIdxHeight * wBitmap.BackBufferStride + nIdxWidth * 4;
-                        byte nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
-
-                        m_nHistgram[0, nGrayScale] += 1;
-
-                        if (m_wbitmap != null)
-                        {
-                            pPixel = (byte*)m_wbitmap.BackBuffer + nIdxHeight * m_wbitmap.BackBufferStride + nIdxWidth * 4;
-                            nGrayScale = (byte)((pPixel[(int)ComInfo.Pixel.B] + pPixel[(int)ComInfo.Pixel.G] + pPixel[(int)ComInfo.Pixel.R]) / 3);
-
-                            m_nHistgram[1, nGrayScale] += 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        public void InitHistgram()
-        {
-            for (int nIdx = 0; nIdx < (m_nHistgram.Length >> 1); nIdx++)
-            {
-                m_nHistgram[0, nIdx] = 0;
-                m_nHistgram[1, nIdx] = 0;
-            }
         }
     }
 }

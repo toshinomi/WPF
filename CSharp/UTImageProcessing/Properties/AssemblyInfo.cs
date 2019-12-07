ecf770a2d0a43c842c6c8a5aa16f06ae96aa24dc
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-12-08 01:25:13 +0900

    Update

---
+++ CSharp/UTImageProcessing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTImageProcessing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTImageProcessing")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("784088d8-80c1-4f64-b8f0-c6b9b0c56028")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharp/UTImageProcessing/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTImageProcessing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Byte nExValue;
+            for (double dValue = -256; dValue < 256;)
+            {
+                if (dValue > 255.0)
+                {
+                    nExValue = 255;
+                }
+                else if (dValue < 0.0)
+                {
+                    nExValue = 0;
+                }
+                else
+                {
+                    nExValue = (byte)dValue;
+                }
+                byte nAns = ComFunc.DoubleToByte(dValue);
+                Assert.AreEqual(nExValue, nAns);
+                dValue += 0.1;
+            }
+        }
+    }
+}
